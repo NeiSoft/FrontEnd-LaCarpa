2d2fe4da5e4cccdff8da8f5aa1655f4ba6e45e94
prox09 <lucas_gonz_98@hotmail.com>
2021-10-23 21:10:50 -0300

    Intento de front

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-
 import Inicio from "./components/Inicio";
+
 import Stock from "./components/Stock";
 import Venta from "./components/Venta";
 
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,17 +1,44 @@
-import React, {useState, useEffect, useRef} from "react";
-import { withRouter } from 'react-router-dom';
-
-
-
-const inicio = () => {
-
-
-
-    
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 
 
 
-}
+const Inicio = () => {
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                News
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      );
+    }
+
+
+
+
+
+export default Inicio;
 
 
